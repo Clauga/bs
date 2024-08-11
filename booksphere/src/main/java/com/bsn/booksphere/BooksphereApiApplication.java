9b2c89207f7384342bf85a20699ad7383e6ed154
clauga <guerrero.ayala.cs@gmail.com>
2024-08-11 16:26:43 +0200

    Add frontend project to monorepo

---
+++ booksphere/src/main/java/com/bsn/booksphere/BooksphereApiApplication.java
@@ -0,0 +1,13 @@
+package com.bsn.booksphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksphereApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksphereApiApplication.class, args);
+	}
+
+}
---
+++ booksphere/src/test/java/com/bsn/booksphere/BooksphereApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsn.booksphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksphereApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
