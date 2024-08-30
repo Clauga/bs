bd4e38ca6765c1b598f79e0a79dfa0a45ce6a155
clauga <guerrero.ayala.cs@gmail.com>
2024-08-30 17:03:13 +0200

    feedback entity backend

---
+++ booksphere/src/main/java/com/bsn/booksphere/feedback/FeedbackMapper.java
@@ -0,0 +1,31 @@
+package com.bsn.booksphere.feedback;
+
+import com.bsn.booksphere.book.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+    //the mappers are used to convert the request and response objects to the entity objects and vice versa
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .shareable(false) // Not required and has no impact :: just to satisfy lombok
+                        .archived(false) // Not required and has no impact :: just to satisfy lombok
+                        .build()
+                )
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer id) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), id))
+                .build();
+    }
+}
